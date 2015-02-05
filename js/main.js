30a3be45961874a3aa0ecd9add2e49c1afb00a0e
jnerad@gmail.com <jnerad@gmail.com>
2015-02-05 00:25:38 -0800

    adding javascript file
    I am sure there is a better way to do this, but this works. Javascript
    to strikethrough list items when checkbox is checked, also toggles back.

---
+++ js/main.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+		// watch to see if checkboxes get checked
+		// $('#one').on('toggle', checkItOff);
+		// // when a checkbox gets checked add the class checked
+		// function checkItOff() {
+		// 		$("#one").toggleClass('checked');
+		// 		console.log("checked")
+		// } 
+	$('#one').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#two').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#three').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#four').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#five').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#six').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#seven').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#eight').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#nine').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#ten').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#eleven').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+	$('#twelve').click(function(){
+	        $(this).toggleClass('checked');  
+	});
+
+});
+
+
